97fc4327114b521d29f8a383d23d6a0baeec22d6
Yuto Tamura <cayt1224@gmail.com>
2023-10-20 08:23:08 +0900

    課題作成

---
+++ main.js
@@ -0,0 +1,19 @@
+//関数宣言
+function showAlert() {
+  alert("アラート表示練習")
+}
+showAlert();
+
+
+//関数式
+var getText = function() {
+  return "関数式練習";
+}
+console.log(getText);
+
+
+/*
+スコープとは
+…変数や関数などを参照できる範囲を決めるもの。
+　スコープ内で定義された変数は、そのスコープ内でのみ参照可能で外側からは参照できない。
+*/
